98dd471f240a7bd83de7d8896560584f5a42cb90
Francisco <frangon152@gmail.com>
2025-10-28 19:25:50 -0300

    remove name

--- myApp.js
+++ myApp.js
@@ -106,8 +106,14 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove},
+  (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+    return done(null, data);
+  });
+  
 };
 
 const queryChain = (done) => {
