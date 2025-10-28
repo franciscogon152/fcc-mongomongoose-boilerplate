c068c2742f89724a6b9de521d4c3d0642703212c
Francisco <frangon152@gmail.com>
2025-10-28 16:42:43 -0300

    find food

--- myApp.js
+++ myApp.js
@@ -48,7 +48,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food},
+  (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+  return done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
