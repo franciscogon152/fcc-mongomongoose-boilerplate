5d908ad6c8d1582f5b8dc99bdd6858f2129bf7f6
Francisco <frangon152@gmail.com>
2025-10-28 13:15:39 -0300

    person

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const PersonSchema = new mongoose.Schema({
+  name: { type : String, required: true},
+  age : Number,
+  favoriteFoods : [String]
+})
+
+const Person = mongoose.model('Person', PersonSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
